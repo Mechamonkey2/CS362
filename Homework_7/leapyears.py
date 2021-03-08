d6a3a9b3d50489e532d2f491cb2a7243bf166994
Duy Pham <notouchieduy@yahoo.com>
2021-03-07 21:57:52 -0800

    Added first test case. mod4

--- Homework_7/leapyears.py
+++ Homework_7/leapyears.py
@@ -1,1 +1,13 @@
+#   Author: Duy Pham
+#   Date: 3/7/2021
+#   Program:leapyears.py
+#   Description: Homework 7
+import unittest
 
+class mod4(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(4,"leap year")
+
+
+if __name__ == "__main__":
+    unittest.main()
