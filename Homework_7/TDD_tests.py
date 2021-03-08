2c692ff3acd70b17221ff204dfd9830d83f75eb3
Duy Pham <notouchieduy@yahoo.com>
2021-03-07 20:52:49 -0800

    Added def return values of fizz, buzz and fizzbuzz

--- Homework_7/TDD_tests.py
+++ Homework_7/TDD_tests.py
@@ -1,2 +1,14 @@
 def hello_world():
     return 'Hello World!'
+
+def FizzBuzz(num):
+    if not num % 15:
+        return "fizzbuzz"
+
+    if not num % 3:
+        return "fizz"
+
+    if not num % 6:
+        return "buzz"
+
+    return num
