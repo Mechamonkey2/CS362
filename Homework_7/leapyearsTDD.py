50152a04ec89000666c0b9565ca83ab67a5e69ac
Duy Pham <notouchieduy@yahoo.com>
2021-03-07 22:48:17 -0800

    Fixed functionality due to testing errors

--- Homework_7/leapyearsTDD.py
+++ Homework_7/leapyearsTDD.py
@@ -1,8 +1,6 @@
 def leapyear(year):
     if (year % 4) == 0:
-        return "leap year"
         if (year % 100) == 0:
-            return "leap year"
             if (year % 400) == 0:
                 return "leap year"
             else:
